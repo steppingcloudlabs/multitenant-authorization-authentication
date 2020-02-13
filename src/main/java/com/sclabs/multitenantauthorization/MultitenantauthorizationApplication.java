7ff7ee49e79bee3ee8d957c6e60ee03f955010fa
maaz1996 <maaz.abdullah111@gmail.com>
2020-02-13 15:29:34 +0530

    project init with spring security

---
+++ src/main/java/com/sclabs/multitenantauthorization/MultitenantauthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.sclabs.multitenantauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantauthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenantauthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sclabs/multitenantauthorization/MultitenantauthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sclabs.multitenantauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantauthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
