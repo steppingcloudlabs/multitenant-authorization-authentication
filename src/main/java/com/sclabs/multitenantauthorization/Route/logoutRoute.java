c6c8794de541f5365519ae21052b7b4143a5c326
maaz1996 <maaz.abdullah111@gmail.com>
2020-02-26 11:36:34 +0530

    logout route added

---
+++ src/main/java/com/sclabs/multitenantauthorization/Route/logoutRoute.java
@@ -0,0 +1,16 @@
+package com.sclabs.multitenantauthorization.Route;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class logoutRoute {
+
+    // @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    /**
+     * Logout Method to be created here
+     */
+    // public String logout(@RequestBody loginSignupModel modeluser) {
+
+    // }
+
+}
